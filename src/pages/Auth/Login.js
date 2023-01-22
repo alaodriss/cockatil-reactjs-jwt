7fc540053e69b6a9bc3540a3ad90972c5fbc3d83
alaodriss <alaodriss@gmail.com>
2023-01-22 13:13:05 +0100

    Branchement du login - Form React et State

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,18 +1,42 @@
-import React from "react";
-import './auth.css'
+import React, { useState } from "react";
+import "./auth.css";
 
 const Login = () => {
+
+
+    // le state ce sont des var qui asyc entre le code et template 
+
+  const [login, setLogin] = useState("");
+  const [password, setpassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("kika");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="group">
         <label htmlFor="login"> Indentifiant</label>
-        <input type="text" name="login" />
+        <input
+          type="text"
+          name="login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
       </div>
       <div className="group">
         <label htmlFor="password"> Mot de Passe </label>
-        <input type="text" name="password" />{" "}
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+        />
       </div>
-      <div className="group"> <button> Connexion </button> </div>
+      <div className="group">
+        <button> Connexion </button>
+      </div>
     </form>
   );
 };
